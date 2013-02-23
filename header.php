52830004c9130154e4dff6ea78e2c05ebb5746ec
Fish Farmer AWS Box <chris@piuggi.com>
2013-02-23 17:08:09 +0000

    Got Typekit working

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo('name'); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<script type="text/javascript" src="//use.typekit.net/anb1nws.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 <script type="text/javascript">
 var theme_dir = "<?php echo get_theme_root(); ?>";
 var theme_url = "<?php echo get_template_directory_uri() ?>";
