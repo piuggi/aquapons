7cdbb551dd727668d1e3f4a2f680adbf657c269a
Fish Farmer AWS Box <chris@piuggi.com>
2013-02-25 21:03:40 +0000

    updating files to current state

--- page-badges-overview.php
+++ page-badges-overview.php
@@ -100,13 +100,13 @@
 				<section class="badges-desc">
 					<section class="badges-desc-content">
 							<p class="main-copy">
-								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionerally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
+								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
 							</p>
 							<p class="body-copy">
-								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionerally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
+								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
 							</p>
 							<p class="body-copy">
-								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionerally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
+								This page explains the hierarchy of badges. To become a Practitioner you must complete certain skill badges. Additionally, you must be assessed by an Aquapons Mentor. Your documentation and community impact will be an integral part of this assessment.
 							</p>
 					</section>
 					<section class="open-badges-callout">
