ecd7c92a93777c83a0d9246c7c5e16380890d380
Fish Farmer AWS Box <chris@piuggi.com>
2013-03-28 17:33:47 +0000

    Fixing Merge issues to styles and structure.

--- templates/badges-single-header.php
+++ templates/badges-single-header.php
@@ -1,7 +1,11 @@
-<?php $theme = new themeCheck(); ?>
+<?php //$theme = new themeCheck(); 
+
+	//global $theme;
+
+ ?>
 
 <div id="sub-nav">
 	<ul id="crumb-nav">
-		<?php breadcrumb($post, $theme); ?>
+		<?php breadcrumb($post); ?>
 	</ul>
 </div><!--#sub-nav-->
